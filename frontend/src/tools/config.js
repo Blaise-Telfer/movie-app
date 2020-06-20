36f1cc5fea9f271192897c151be787e46ad32bec
Blaise-Telfer <56236726+Blaise-Telfer@users.noreply.github.com>
2020-06-20 10:02:55 -0700

    Add files via upload

---
+++ frontend/src/tools/config.js
@@ -0,0 +1,7 @@
+//server route
+export const USER_SERVER = '/api/users';
+
+//api keys
+export const API_KEY = "24e511390bdafcd017a67405c0d07b9f";
+export const API_URL = "https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/";
+export const IMAGE_URL = "https://image.tmdb.org/t/p/";
